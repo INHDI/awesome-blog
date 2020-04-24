744472e079761eda85e438ab6debefeaa07d7f0a
NHD_11 <dang12.10.1999@gmail.com>
2020-04-24 20:56:32 +0700

    NHD

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn awesomenlog.wsgi --log-file
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\nhd01\PycharmProjects\WebSever\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
